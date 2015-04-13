b80b68fd1c255e4553096b4a4136b0d24af17380
Romer Ramos <rramos@4geeks.co>
2015-04-12 23:29:14 -0430

    Creación de indicador de entrega utilizando el id del reto y el github username

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,12 +4,15 @@ class ChallengesController < ApplicationController
 
   def show
     @challenge_repo = nil
+    @challenge_url = nil
     @challenge = Challenge.find params[:id]
     day = @challenge.day
     week = day.week
+    challenge = params[:id]
 
     if current_user.github_user
-      @challenge_repo = "w_#{week.number}_d_#{day.number}_#{current_user.github_user}"
+      @challenge_repo = "w_#{week.number}_d_#{day.number}_ch#{challenge}_#{current_user.github_user}"
+      @challenge_url = "https://github.com/Academia-Hack-Promo2/#{@challenge_repo}"
     end
   end
 
