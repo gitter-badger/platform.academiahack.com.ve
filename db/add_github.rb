7aabb4b48fa56948437e344020ea99a0d3350770
Romer Ramos <rramos@4geeks.co>
2015-04-13 01:01:13 -0430

    Listado de usuarios de github de la promo2

---
+++ db/add_github.rb
@@ -0,0 +1,47 @@
+user = User.find_by_email 'maldonmarco@gmail.com'
+user.github_user = 'mmaldonado730'
+user.save
+
+user = User.find_by_email 'anggelica.virtlife.jt@gmail.com'
+user.github_user = 'aherrera768'
+user.save
+
+user = User.find_by_email 'gen.reyest@gmail.com'
+user.github_user = 'greyes193'
+user.save
+
+user = User.find_by_email 'gamezh81@gmail.com'
+user.github_user = 'hgamez011'
+user.save
+
+user = User.find_by_email 'jamm@jmartinezm.org'
+user.github_user = 'jmartinez519'
+user.save
+
+user = User.find_by_email 'samuel_021093@hotmail.com'
+user.github_user = 'smelgarejo771'
+user.save
+
+user = User.find_by_email 'locke_din15@hotmail.com'
+user.github_user = 'llopez538'
+user.save
+
+user = User.find_by_email 'luisdavidrn@gmail.com'
+user.github_user = 'lrodriguez194'
+user.save
+
+user = User.find_by_email 'hector12j@gmail.com'
+user.github_user = 'hcarreno389'
+user.save
+
+user = User.find_by_email 'alexballera@gmail.com'
+user.github_user = 'aballera523'
+user.save
+
+user = User.find_by_email 'galindezj2@gmail.com'
+user.github_user = 'jgalindez057'
+user.save
+
+user = User.find_by_email 'mariiromero17@gmail.com'
+user.github_user = 'mromero942'
+user.save
