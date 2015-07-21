f28504288dd7e80415f362df1e083344c3581e46
Romer Ramos <rramos@4geeks.co>
2015-07-21 22:37:32 +0000

    Agregado archivo de enviroment keys a linked files en capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:HackAcademy/plataforma.hack4geeks.co.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/enviroment_variables.yml')
+set :linked_files, fetch(:linked_files, []).push('config/environment_variables.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
