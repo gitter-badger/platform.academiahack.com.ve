9a4c790da53c0b68aacf77d05d4e4f47f9f25657
romerramos@gmail.com <romerramos@gmail.com>
2015-08-16 23:49:05 -0430

    Agregada info deployment al server nuevo

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,12 +1,16 @@
-server '45.55.172.121', user: 'deploy', roles: %w{web app}
+# server '45.55.172.121', user: 'deploy', roles: %w{web app}
+server '45.55.165.253', user: 'deploy', roles: %w{web app}
 
-role :app, %w{deploy@45.55.172.121}
-role :web, %w{deploy@45.55.172.121}
-role :db,  %w{deploy@45.55.172.121}
+
+# role :app, %w{deploy@45.55.172.121}
+# role :web, %w{deploy@45.55.172.121}
+# role :db,  %w{deploy@45.55.172.121}
+
+role :app, %w{deploy@45.55.165.253}
+role :web, %w{deploy@45.55.165.253}
+role :db,  %w{deploy@45.55.165.253}
 
 set :stage, :development
 set :branch, 'development'
 
-server '45.55.172.121', user: 'deploy', roles: %w{web app}
-
-set :deploy_to, '/home/deploy/dev.platform.hack4geeks.co'
+set :deploy_to, '/home/deploy/dev.platform.academiahack.com.ve'
